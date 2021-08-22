30ce451efec52ce6eb8f1c3e01eb6394a8fe0372
MINSK\Yuliya_Kryvarot <yuliya_kryvarot@epam.com>
2021-08-22 22:59:45 +0300

    feat: improve accessibility using JS

--- js/custom.js
+++ js/custom.js
@@ -7,34 +7,99 @@
   });
 })();
 
-document.querySelectorAll('#nav li').forEach(function (navEl) {
-  navEl.onclick = function () {
-    toggleTab(this.id, this.dataset.target);
-  };
+const tabs = document.querySelectorAll('[role="tab"]');
+
+tabs.forEach(function (tab) {
+  tab.addEventListener('click', toggleTab);
+  tab.addEventListener('keyup', toggleTab);
+  tab.addEventListener('keydown', toggleTab);
 });
 
-function toggleTab(selectedNav, targetId) {
-  var navEls = document.querySelectorAll('#nav li');
+function toggleTab(event) {
+  const { code, type } = event;
+  const currentTab = document
+    .querySelector('[role="tablist"]')
+    .querySelector('[aria-selected="true"]');
 
-  navEls.forEach(function (navEl) {
-    if (navEl.id == selectedNav) {
-      navEl.classList.add('is-active');
-    } else {
-      if (navEl.classList.contains('is-active')) {
-        navEl.classList.remove('is-active');
+  if (type.match('click')) {
+    const selectedTab = event.currentTarget.id;
+
+    tabs.forEach((tab) => {
+      tab.id === selectedTab ? activateTab(tab) : deactivateTab(tab);
+    });
+    toggleTabPanels(event.currentTarget);
+  }
+
+  if (type.match('keyup')) {
+    if (code === 'ArrowLeft' || code === 'ArrowRight') {
+      const direction = {
+        ArrowLeft: -1,
+        ArrowRight: 1,
+      };
+
+      event.preventDefault();
+      const index = currentTab.id - 1;
+
+      deactivateTab(currentTab);
+
+      if (tabs[index + direction[code]]) {
+        activateTab(tabs[index + direction[code]]);
+      } else if (code === 'ArrowRight') {
+        activateFirst();
+      } else {
+        activateLast();
       }
     }
-  });
 
-  var tabs = document.querySelectorAll('.tab-pane');
-
-  tabs.forEach(function (tab) {
-    if (tab.id == targetId) {
-      tab.style.display = 'block';
-    } else {
-      tab.style.display = 'none';
+    if (code === 'Space' || code === 'Enter') {
+      toggleTabPanels(event.currentTarget);
     }
+  }
+
+  if (type.match('keydown')) {
+    if (code === 'Home' || code === 'End') {
+      event.preventDefault();
+      deactivateTab(currentTab);
+
+      switch (code) {
+        case 'Home':
+          activateFirst();
+          break;
+        case 'End':
+          activateLast();
+          break;
+      }
+    }
+  }
+}
+
+function activateFirst() {
+  activateTab(tabs[0]);
+}
+
+function activateLast() {
+  activateTab(tabs[tabs.length - 1]);
+}
+
+function activateTab(tab) {
+  tab.removeAttribute('tabindex');
+  tab.setAttribute('aria-selected', 'true');
+  tab.focus();
+}
+
+function deactivateTab(tab) {
+  tab.setAttribute('tabindex', '-1');
+  tab.setAttribute('aria-selected', 'false');
+  tab.blur();
+}
+
+function toggleTabPanels(selected) {
+  document.querySelectorAll('[role="tabpanel"]').forEach((el) => {
+    el.style.display = 'none';
   });
+
+  const controls = selected.getAttribute('aria-controls');
+  document.getElementById(controls).style.display = 'block';
 }
 
 const menuEl = document.querySelector('.dropMenu-menu');
@@ -69,3 +134,118 @@ function displayRandomImage() {
 }
 
 setInterval(displayRandomImage, 60000);
+
+const toggleExpandedMenuElem = document.getElementById('expanded-menu-item');
+const expandedMenuElem = document.querySelector('#expanded-menu-item > ul');
+const expandedMenuItemsElems = document.querySelectorAll(
+  '#expanded-menu-item [role="menuitem"]'
+);
+
+toggleExpandedMenuElem.addEventListener('click', toggleMenu);
+
+toggleExpandedMenuElem.addEventListener('keydown', (e) => {
+  const { code } = e;
+
+  const expanded =
+    toggleExpandedMenuElem.getAttribute('aria-expanded') === 'true';
+
+  switch (code) {
+    case 'Space':
+    case 'Enter':
+      e.preventDefault();
+      if (!expanded) {
+        openMenu();
+      } else {
+        alert(document.activeElement.textContent);
+        closeMenu();
+      }
+      break;
+    case 'Escape':
+      e.preventDefault();
+      closeMenu();
+      break;
+    case 'ArrowRight':
+      e.preventDefault();
+      closeMenu();
+      document.querySelector('#navbarMenu > ul > li:nth-child(3) > a').focus();
+      break;
+    case 'ArrowLeft':
+      e.preventDefault();
+      closeMenu();
+      document.querySelector('#navbarMenu > ul > li:nth-child(1) > a').focus();
+      break;
+    case 'ArrowDown':
+      if (expanded) {
+        e.preventDefault();
+
+        const current = document.activeElement;
+        const index = +current?.dataset?.index;
+        const { length } = expandedMenuItemsElems;
+
+        current.blur();
+
+        const target =
+          index + 1 < length
+            ? expandedMenuItemsElems[index + 1]
+            : expandedMenuItemsElems[0];
+        target.focus();
+      }
+      break;
+    case 'ArrowUp':
+      if (expanded) {
+        e.preventDefault();
+
+        const current = document.activeElement;
+        const index = +current?.dataset?.index;
+        const { length } = expandedMenuItemsElems;
+
+        current.blur();
+
+        const target =
+          index - 1 >= 0
+            ? expandedMenuItemsElems[index - 1]
+            : expandedMenuItemsElems[length - 1];
+        target.focus();
+      }
+      break;
+    case 'Home':
+      if (expanded) {
+        e.preventDefault();
+
+        const current = document.activeElement;
+        current.blur();
+
+        expandedMenuItemsElems[0].focus();
+      }
+      break;
+    case 'End':
+      if (expanded) {
+        e.preventDefault();
+
+        const current = document.activeElement;
+        current.blur();
+
+        expandedMenuItemsElems[expandedMenuItemsElems.length - 1].focus();
+      }
+      break;
+  }
+});
+
+function toggleMenu() {
+  if (toggleExpandedMenuElem.getAttribute('aria-expanded') === 'true') {
+    closeMenu();
+  } else {
+    openMenu();
+  }
+}
+
+function openMenu() {
+  toggleExpandedMenuElem.setAttribute('aria-expanded', 'true');
+  expandedMenuElem.style.display = 'block';
+  expandedMenuItemsElems[0].focus();
+}
+
+function closeMenu() {
+  toggleExpandedMenuElem.setAttribute('aria-expanded', 'false');
+  expandedMenuElem.style.display = 'none';
+}
